e08c7a7ec02059ed80d14c4c5d8d5185fcff0417
PAU MONTERDE OLMOS <pamool@floridauniversitaria.es>
2021-02-03 19:58:58 +0100

    0.4-SNAPSHOT: Profile

--- screens/Main.js
+++ screens/Main.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
+import { SearchBar, ButtonGroup } from 'react-native-elements';
 
 
 
@@ -13,15 +14,53 @@ import {
 import {Button} from 'react-native-elements'
 
 export class Main extends Component {
+  constructor () {
+    super()
+    this.state = {
+      selectedIndex: 3
+    }
+    this.updateIndex = this.updateIndex.bind(this)
+  }
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex})
+  }
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+
   render() {
+    const component1 = () => <Text style={{fontFamily: 'Arial', fontSize: 22}} onPress={()=>this.props.navigation.navigate('BookList')}>Library</Text>
+    const component2 = () => <Text style={{fontFamily: 'Arial', fontSize: 22}} onPress={()=>this.props.navigation.navigate('AddBook')}>Add</Text>
+    const component3 = () => <Text style={{fontFamily: 'Arial', fontSize: 22}} onPress={()=>this.props.navigation.navigate('Profile')}>Profile</Text>
+    const { search } = this.state;
+    const buttons = [{ element: component1 }, { element: component2 }, { element: component3 }]
+  const { selectedIndex } = this.state
+
     return (
-        <View>
-          <StatusBar barStyle="dark-content" />
-          <View >
-              <Text> Main </Text>
-              <Button title='Profile' onPress={()=>this.props.navigation.navigate('Profile')} />
-          </View>
+      <View style={{flex: 1}}>
+      
+      <SearchBar
+      inputContainerStyle={{backgroundColor: 'white'}}
+      leftIconContainerStyle={{backgroundColor: 'white'}}
+      inputStyle={{backgroundColor: 'white'}}
+      containerStyle={{
+        backgroundColor: '#81D3F8',
+        justifyContent: 'space-around',
+        borderTopWidth:0,
+        borderBottomWidth:0}}
+      />
+      <View style={{ position: 'absolute', width:'100%',bottom: 0,flex: 0.5 }}>
+        <ButtonGroup
+        onPress={this.updateIndex}
+        selectedIndex={selectedIndex}
+        buttons={buttons}
+        containerStyle={{height: 75, backgroundColor: '#81D3F8'}} />
         </View>
+      </View>
     );
   }
 };
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,33 +1,76 @@
 import React, { Component } from 'react';
-import {StyleSheet,View,Button,Text,StatusBar,} from 'react-native';
-import { Input,Icon,} from 'react-native-elements';
+import {StyleSheet,View,Text,StatusBar,} from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import {Icon} from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Help } from './Help';
+import { MyAds } from './MyAds';
+import { EditProfile } from './EditProfile';
+import { Favorites } from './Favorites';
+import { Chat } from './Chat';
+import { Login } from './Login';
+import { ScrollView } from 'react-native-gesture-handler';
+import { abs } from 'react-native-reanimated';
 
 export class Profile extends Component{
     render() {
         return (
+            <ScrollView>
             <View style={{flex: 1, flexDirection:'column',backgroundColor: 'white', alignItems: 'center', alignContent: 'center'}}>
                 <StatusBar barStyle="dark-content" />
                 <View style={{margin:50}}>
-                    <Text style={{fontFamily: 'Arial', fontSize: 32, textAlign: 'center',color: '#333333',flex: 0.55}}> Log in and start discovering </Text>
-                    <View style={{flex: 1}}>
-                    <Input
-                        placeholder='Email'
-                        leftIcon={{ name: 'mail' }}
-                    />
-                    <Input 
-                        placeholder='Password'
-                        leftIcon={{ name: 'lock' }}
-                    />
-                    <Text style={{textAlign: 'right',fontFamily: 'Arial', fontSize: 18,color: '#169BD5'}} onPress={()=>this.props.navigation.navigate('Password')}>Don't remember your password?</Text>
-                </View>
-                    <Button title='Continue' onPress={()=>this.props.navigation.navigate('Help')} />
-                    <Text style={{textAlign: 'center',fontFamily: 'Arial', fontSize: 18}}>You don't have an account?</Text>
-                    <Text style={{textAlign: 'center',fontFamily: 'Arial', fontSize: 18,color: '#169BD5'}} onPress={()=>this.props.navigation.navigate('Register')}>Sign in</Text>
-                </View>
+                    <Text style={{fontFamily: 'Arial', fontSize: 32, textAlign: 'center',color: '#333333',flex: 0.55}}> Profile and Settings </Text>
+                    <View style={styles.contenidor}>
+            <View style={styles.seccio1}>
+                <Button icon={
+                        <Icon name="chat" type size={30} color="#000000"></Icon>
+                }
+                iconContainerStyle={{margin: 20}}
+                titleStyle={{color:"#000000"}} title='Chat' onPress={()=>this.props.navigation.navigate('Chat')} > </Button>
             </View>
+            <View style={styles.seccio2}>
+                <Button icon={
+                        <Icon name="drive-folder-upload" type size={30} color="#000000"></Icon>
+                }
+                iconContainerStyle={{margin: 20}}
+                titleStyle={{color:"#000000"}} title='MyAds' onPress={()=>this.props.navigation.navigate('MyAds')} > </Button>
+            </View>
+            <View style={styles.seccio3}>
+                <Button icon={
+                        <Icon name="favorite" type size={30} color="#000000"></Icon>
+                }
+                iconContainerStyle={{margin: 20}}
+                titleStyle={{color:"#000000"}} title='Favorites' onPress={()=>this.props.navigation.navigate('Favorites')} > </Button>
+            </View>
+            <View style={styles.seccio4}>
+                <Button icon={
+                        <Icon name="exit-to-app" type size={30} color="#000000"></Icon>
+                }
+                iconContainerStyle={{margin: 20}}
+                titleStyle={{color:"#000000"}} title='Exit' onPress={()=>this.props.navigation.navigate('Login')} > </Button>
+            </View>
+            <View style={styles.seccio5}>
+                <Button icon={
+                        <Icon 
+                        name="create"
+                        size={30} color="#000000"></Icon>
+                }
+                iconContainerStyle={{margin: 20}}
+                titleStyle={{color:"#000000"}} title='EditProfile' onPress={()=>this.props.navigation.navigate('EditProfile')} > </Button>
+            </View>
+            <View style={styles.seccio6}>
+                <Button icon={
+                        <Icon name="help" type size={30} color="#000000"></Icon>
+                }
+                iconContainerStyle={{margin: 20}}
+                titleStyle={{color:"#000000"}} title='Help' onPress={()=>this.props.navigation.navigate('Help')} > </Button>
+               
+            </View>
+            </View>                       
+              </View>
+            </View>
+            </ScrollView>
         );
     }
 }
@@ -43,6 +86,59 @@ const styles = StyleSheet.create({
         margin: 5,
         alignContent: 'space-around',
         alignItems: 'center',
-    },
-
+        },
+        seccio1: {
+        flex:1,
+        fontSize: 12,
+        fontWeight: '600',
+        width: 350,
+        margin: 4,
+        padding: 12,
+        textAlign: 'right',
+        },
+        seccio2: {
+        flex:1,
+        fontSize: 350,
+        fontWeight: '600',
+        width: 350,
+        margin: 4,
+        padding: 12,
+        textAlign: 'center',
+        },
+        seccio3: {
+        flex:1,
+        fontSize: 350,
+        fontWeight: '600',
+        width: 350,
+        margin: 4,
+        padding: 12,
+        textAlign: 'left',    
+        },
+        seccio4: {
+        flex:1,
+        fontSize: 350,
+        fontWeight: '600',
+        width: 350,
+        margin: 4,
+        padding: 12,
+        textAlign: 'left',    
+        },
+        seccio5: {
+        flex:1,
+        fontSize: 350,
+        fontWeight: '600',
+        width: 350,
+        margin: 4,
+        padding: 12,
+        textAlign: 'left',    
+        },
+        seccio6: {
+        flex:1,
+        fontSize: 350,
+        fontWeight: '600',
+        width: 350,
+        margin: 4,
+        padding: 12,
+        textAlign: 'left',  
+        },
 });
